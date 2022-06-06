f1321016d36f4fe9d574cfd77dc282f31d1631f4
vahid nikforuz <57806426+Vahid2001-nikforuz@users.noreply.github.com>
2022-06-06 18:09:29 +0430

    Update input.jsx

--- src/component/input.jsx
+++ src/component/input.jsx
@@ -2,8 +2,8 @@ import React from "react";
 function Input(props) {
     let text = "Enter Instagram or YouTube or TikTok acount";
     return (
-        <div class="inputclass">
-            <input type={"input"} placeHolder={text} />
+        <div className="inputclass">
+            <input type={"input"} placeholder={text} />
             <button>CLick me</button>
         </div>
     )
